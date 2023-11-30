952d98fb6fb4471b5041380bb78010f753e46280
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-30 23:06:46 +0700

    index.js input
    index.js input

---
+++ Tugas34.js
@@ -0,0 +1,14 @@
+function Tugas34() {
+      
+      // const leg = ['input'];
+
+      // for (let i = 0; i < leg.length; i++) {
+      //       const element = leg[i];
+            
+      //       console.log(element);
+      // }
+
+      console.log(document.getElementsByTagName('input'));
+
+}
+Tugas34();
